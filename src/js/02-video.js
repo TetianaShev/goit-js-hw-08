c2f49fc843003261b215d3177773153e9ebfa2c8
TetianaShev <tanishika362@gmail.com>
2023-11-03 23:56:29 +0200

    editing
    editing

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,35 @@
 import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
 const currentTimeKey = 'videoplayer-current-time';
 
-const iframe = document.querySelector('#vimeo-player');
-const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-function timeUpdate(event) {
-    localStorage.setItem(currentTimeKey, event.seconds);
-}
+player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem(currentTimeKey, JSON.stringify(data.seconds))
+  }, 1000));
 
-player.setCurrentTime(
-    localStorage.getItem(currentTimeKey) ? localStorage.getItem(currentTimeKey) : 0,
-);
-player.on('timeupdate', throttle(timeUpdate, 1000));
+  player.setCurrentTime(localStorage.getItem(currentTimeKey)).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
+// function timeUpdate(event) {
+//     localStorage.setItem(currentTimeKey, event.seconds);
+// }
+
+// player.setCurrentTime(
+//     localStorage.getItem(currentTimeKey) ? localStorage.getItem(currentTimeKey) : 0,
+// );
+// player.on('timeupdate', throttle(timeUpdate, 1000));
